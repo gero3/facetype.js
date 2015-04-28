6c22bc089e44044ac6b78fa35ce0079a326514e9
gero3 <jeroen-876@hotmail.com>
2015-04-28 22:45:01 +0200

    create paths correctly
    In particularly bezier curves have 3 points.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,80 +1,16 @@
-var submit = document.getElementById("submit");
+var convert = document.getElementById("convert");
 var fileInput = document.getElementById("fileInput");
-submit.onclick = function(){
+convert.onclick = function(){
 
     [].forEach.call(fileInput.files,function(file){
         var reader = new FileReader();
         reader.addEventListener( 'load', function ( event ) {
-
             var font = opentype.parse(event.target.result);
-            console.log(font);
-
-            var scale = (1000 * 100) / (2048 *72);
-            var result = {};
-            result.glyphs = {};
-
-            font.glyphs.forEach(function(glyph){
-                if (glyph.unicode !== undefined) {
-                    if (String.fromCharCode(glyph.unicode) === "0"){
-                        console.log(glyph.getPath(0,0,100));
-                    };
-
-                    var token = {};
-                    token.ha = Math.round(glyph.advanceWidth * scale);
-                    token.x_min = Math.round(glyph.xMin * scale);
-                    token.x_max = Math.round(glyph.xMax * scale);
-                    token.o = ""
-                    glyph.path.commands.forEach(function(command){
-                        if (command.type.toLowerCase() === "z") {return;}
-                        token.o += command.type.toLowerCase();
-                        token.o += " "
-                        token.o += Math.round(command.x * scale);
-                        token.o += " "
-                        token.o += Math.round(command.y * scale);
-                        token.o += " "
-                        if (command.x1 !== undefined && command.y1 !== undefined){
-                            token.o += Math.round(command.x1 * scale);
-                            token.o += " "
-                            token.o += Math.round(command.y1 * scale);
-                            token.o += " "
-                        }
-                    });
-
-                    result.glyphs[String.fromCharCode(glyph.unicode)] = token;
-                };
-            });
-            result.familyName = font.familyName;
-            result.ascender = Math.round(font.ascender * scale);
-            result.descender = Math.round(font.descender * scale);
-            result.underlinePosition = font.tables.post.underlinePosition;
-            result.underlineThickness = font.tables.post.underlineThickness;
-            result.boundingBox = {
-                "yMin": font.tables.head.yMin,
-                "xMin": font.tables.head.xMin,
-                "yMax": font.tables.head.yMax,
-                "xMax": font.tables.head.xMax
-            };
-            result.resolution = 1000;
-            result.original_font_information = font.tables.name;
-            if (font.styleName.toLowerCase().indexOf("bold") > -1){
-                result.cssFontWeight = "bold";
-            } else {
-                result.cssFontWeight = "normal";
-            };
-
-            if (font.styleName.toLowerCase().indexOf("italic") > -1){
-                result.cssFontStyle = "italic";
-            } else {
-                result.cssFontStyle = "normal";
-            };
-
-            exportString("if (_typeface_js && _typeface_js.loadFace) _typeface_js.loadFace("+ JSON.stringify(result) + ");",font.familyName + "_" + font.styleName  + ".js");
+            var result = convert(font);
+            exportString(result,font.familyName + "_" + font.styleName  + ".js");
         }, false );
         reader.readAsArrayBuffer( file );
     });
-
-
-
 };
 
 var exportString = function ( output, filename ) {
@@ -89,3 +25,72 @@ var exportString = function ( output, filename ) {
 		link.click();
 
 	};
+
+var convert =function(font){
+
+    console.log(font);
+
+    var scale = (1000 * 100) / ( (font.unitsPerEm || 2048) *72);
+    var result = {};
+    result.glyphs = {};
+
+    font.glyphs.forEach(function(glyph){
+        if (glyph.unicode !== undefined) {
+
+            var token = {};
+            token.ha = Math.round(glyph.advanceWidth * scale);
+            token.x_min = Math.round(glyph.xMin * scale);
+            token.x_max = Math.round(glyph.xMax * scale);
+            token.o = ""
+            glyph.path.commands.forEach(function(command,i){
+                if (command.type.toLowerCase() === "c") {command.type = "b";}
+                token.o += command.type.toLowerCase();
+                token.o += " "
+                if (command.x !== undefined && command.y !== undefined){
+                    token.o += Math.round(command.x * scale);
+                    token.o += " "
+                    token.o += Math.round(command.y * scale);
+                    token.o += " "
+                }
+                if (command.x1 !== undefined && command.y1 !== undefined){
+                    token.o += Math.round(command.x1 * scale);
+                    token.o += " "
+                    token.o += Math.round(command.y1 * scale);
+                    token.o += " "
+                }
+                if (command.x2 !== undefined && command.y2 !== undefined){
+                    token.o += Math.round(command.x2 * scale);
+                    token.o += " "
+                    token.o += Math.round(command.y2 * scale);
+                    token.o += " "
+                }
+            });
+            result.glyphs[String.fromCharCode(glyph.unicode)] = token;
+        };
+    });
+    result.familyName = font.familyName;
+    result.ascender = Math.round(font.ascender * scale);
+    result.descender = Math.round(font.descender * scale);
+    result.underlinePosition = font.tables.post.underlinePosition;
+    result.underlineThickness = font.tables.post.underlineThickness;
+    result.boundingBox = {
+        "yMin": font.tables.head.yMin,
+        "xMin": font.tables.head.xMin,
+        "yMax": font.tables.head.yMax,
+        "xMax": font.tables.head.xMax
+    };
+    result.resolution = 1000;
+    result.original_font_information = font.tables.name;
+    if (font.styleName.toLowerCase().indexOf("bold") > -1){
+        result.cssFontWeight = "bold";
+    } else {
+        result.cssFontWeight = "normal";
+    };
+
+    if (font.styleName.toLowerCase().indexOf("italic") > -1){
+        result.cssFontStyle = "italic";
+    } else {
+        result.cssFontStyle = "normal";
+    };
+    return "if (_typeface_js && _typeface_js.loadFace) _typeface_js.loadFace("+ JSON.stringify(result) + ");"
+};
